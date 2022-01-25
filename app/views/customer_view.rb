863b63cd6d184e4c98b78a7a43734c7219c49393
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-01-25 17:58:36 +0100

    Created views

---
+++ app/views/customer_view.rb
@@ -0,0 +1,12 @@
+class CustomerView
+  def ask_user_for(stuff)
+    puts "#{stuff}?"
+    print '> '
+
+    gets.chomp
+  end
+
+  def display(customers)
+    customers.each { |customer| puts "#{customer.name} 🏡 #{customer.address}" }
+  end
+end
---
+++ app/views/meal_view.rb
@@ -0,0 +1,12 @@
+class MealView
+  def ask_user_for(stuff)
+    puts "#{stuff}?"
+    print '> '
+
+    gets.chomp
+  end
+
+  def display(meals)
+    meals.each { |meal| puts "#{meal.name} 💵 #{meal.price}" }
+  end
+end
