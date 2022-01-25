d8b80c4cf0fbfaa2c5e87562a88b480686f9d053
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-01-25 17:57:56 +0100

    Created the customer_controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,24 @@
+require_relative '../views/customer_view'
+require_relative '../models/customer'
+
+class CustomersController
+  def initialize(customer_repository)
+    @customer_repository = customer_repository
+    @view = CustomerView.new
+  end
+
+  def add
+    name = @view.ask_user_for(:name)
+    address = @view.ask_user_for(:address)
+
+    customer = Customer.new(name: name, address: address)
+
+    @customer_repository.create(customer)
+  end
+
+  def list
+    customers = @customer_repository.all
+
+    @view.display(customers)
+  end
+end
