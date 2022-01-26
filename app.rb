a41f5fa9eaaf2e913c436151002eda6ef4f66c9c
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-01-26 10:04:43 +0100

    Added necessary imports

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+require_relative 'app/repositories/employees_repository'
+require_relative 'app/controllers/sessions_controller'
+
 require_relative 'app/repositories/meals_repository'
 require_relative 'app/controllers/meals_controller'
 
@@ -8,12 +11,16 @@ require_relative 'router'
 
 MEALS_CSV_FILE = File.join(__dir__, 'data/meals.csv')
 CUSTOMERS_CSV_FILE = File.join(__dir__, 'data/customers.csv')
+EMPLOYEES_CSV_FILE = File.join(__dir__, 'data/employees.csv')
 
-meals_repository = MealRepository.new(MEALS_CSV_FILE)
+employees_repository = EmployeesRepository.new(EMPLOYEES_CSV_FILE)
+sessions_controller = SessionsController.new(employees_repository)
+
+meals_repository = MealsRepository.new(MEALS_CSV_FILE)
 meals_controller = MealsController.new(meals_repository)
 
-customers_repository = CustomerRepository.new(CUSTOMERS_CSV_FILE)
+customers_repository = CustomersRepository.new(CUSTOMERS_CSV_FILE)
 customers_controller = CustomersController.new(customers_repository)
 
-router = Router.new(meals_controller, customers_controller)
+router = Router.new(meals_controller, customers_controller, sessions_controller)
 router.run
