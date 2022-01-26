dd571194252a827c54be508446d9dd5e8bad9068
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-01-26 09:37:19 +0100

    Created the sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController
+  def initialize(employees_repository)
+    @employees_repository = employees_repository
+    @view = SessionsView.new
+  end
+
+  def sign_in
+    # 1. Ask user for
+    username = @view.ask_user_for(:username)
+
+    # 2. Ask user for password
+    password = @view.ask_user_for(:password)
+
+    # 3. Find user by username
+    employee = @employees_repository.find_by_username(username)
+
+    # 4. Match the password
+    if employee && employee.password == password
+      @view.valid_credentials(username)
+    else
+      @view.invalid_credentials
+    end
+  end
+end
